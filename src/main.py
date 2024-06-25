a17c5c2e7542cb4415343092e0025d530d711390
VladosIncorparated <podorvan03@mail.ru>
2024-06-26 06:39:10 +0900

    CORS

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
 from .database.session_database import get_session
 from .api.router import router
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:8000",
+    "http://localhost:8001",
+    "http://localhost:8002",
+    "http://localhost:8003"
+]
+
 
 app= FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
