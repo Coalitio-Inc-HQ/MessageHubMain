4d02d955152c299f03d6423e4a64c6fe85224218
VladosIncorparated <podorvan03@mail.ru>
2024-06-26 21:01:40 +0900

    docker

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.12
+RUN mkdir /app
+WORKDIR /app
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+COPY . .
+RUN chmod a+x *.sh
--- run_seerver.py
+++ run_seerver.py
@@ -2,4 +2,5 @@ import uvicorn
 from src.main import app
 
 uvicorn.run(app, host="127.0.0.1", port=8001)
+# uvicorn.run(app, host="0.0.0.0", port=8001)
 # 0.0.0.0 для docker
