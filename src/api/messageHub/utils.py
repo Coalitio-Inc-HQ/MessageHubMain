c2f9cd13f71ca916f53ee55f0ae17e2ef4170dcc
VladosIncorparated <podorvan03@mail.ru>
2025-02-12 18:44:07 +0900

    add retruse

--- src/api/messageHub/utils.py
+++ src/api/messageHub/utils.py
@@ -6,32 +6,41 @@ from typing import Any
 from pydantic import BaseModel
 from src.settings import settings
 
+import asyncio
+
+retries = 5
+delay = 1
+
 async def send_http_request(base_url:str, relative_url:str, json: Any|None):
     async with AsyncClient(base_url=base_url) as clinet:
-        try:
-            response = await clinet.post(relative_url, json=json, headers={"API-KEY": settings.OUT_API_KEY})
-            response.raise_for_status()
-        except HTTPStatusError as error:
+        for attempt in range(1, retries + 1):
             try:
-                response_data = error.response.json()
-            except:
-                response_data = None
-            log(LogMessage(time=None,heder=f"Ошибка запроса. {error.response.status_code}",
-                   heder_dict=error.args,body=
-                    {
-                        "base_url":base_url, 
-                        "relative_url":relative_url, 
-                        "json":json,
-                        "response":response_data,
-                    },
-                    level=log_en.ERROR))
-        except Exception as error:
-            log(LogMessage(time=None,heder="Неизвестная ошибка.", 
-                   heder_dict=error.args,body=
-                    {
-                        "base_url":base_url, 
-                        "relative_url":relative_url, 
-                        "json":json
-                    },
-                    level=log_en.ERROR))
-            
+                response = await clinet.post(relative_url, json=json, headers={"API-KEY": settings.OUT_API_KEY})
+                response.raise_for_status()
+                return response.json()
+            except HTTPStatusError as error:
+                try:
+                    response_data = error.response.json()
+                except:
+                    response_data = None
+                log(LogMessage(time=None,heder=f"Ошибка запроса. {error.response.status_code}",
+                    heder_dict=error.args,body=
+                        {
+                            "base_url":base_url, 
+                            "relative_url":relative_url, 
+                            "json":json,
+                            "response":response_data,
+                        },
+                        level=log_en.ERROR))
+            except Exception as error:
+                log(LogMessage(time=None,heder="Неизвестная ошибка.", 
+                    heder_dict=error.args,body=
+                        {
+                            "base_url":base_url, 
+                            "relative_url":relative_url, 
+                            "json":json
+                        },
+                        level=log_en.ERROR))
+                
+            if attempt < retries:
+                await asyncio.sleep(delay * (2 ** (attempt - 1)))
